4151bd2c138ae4afae0a5cb368f6052adc385773
Wangjarimm <karamissuu@gmail.com>
2024-10-11 19:29:55 +0700

    buat homepage

---
+++ nasgorid_fe/dist/js/script.js
@@ -0,0 +1,29 @@
+// document.getElementById('starters').addEventListener('click', function () {
+//     document.getElementById('starterSection').classList.remove('hidden');
+//     document.getElementById('lunchSection').classList.add('hidden');
+// });
+
+// // Fungsi untuk menangani klik pada tombol Lunch
+// document.getElementById('lunch').addEventListener('click', function () {
+//     document.getElementById('lunchSection').classList.remove('hidden');
+//     document.getElementById('starterSection').classList.add('hidden');
+// });
+
+// window.addEventListener('scroll', () => {
+//     const header = document.querySelector('header');
+//     const fixedNav = header.offsetTop;
+
+//     if (window.pageYOffset > fixedNav) {
+//         header.classList.add('navbar-fixed');
+//     } else {
+//         header.classList.remove('navbar-fixed');
+//     }
+// });
+
+// const hamburger = document.querySelector('#hamburger');
+// const navMenu = document.querySelector('#nav-menu');
+
+// hamburger.addEventListener('click', () => {
+//     hamburger.classList.toggle('hamburger-active');
+//     navMenu.classList.toggle('hidden');
+// });
