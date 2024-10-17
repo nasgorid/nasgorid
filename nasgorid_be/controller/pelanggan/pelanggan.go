318335ec732de43ce160224d3b267a6028b70caf
Wangjarimm <karamissuu@gmail.com>
2024-10-17 08:28:30 +0700

    add func get pelanggan by id

--- nasgorid_be/controller/pelanggan/pelanggan.go
+++ nasgorid_be/controller/pelanggan/pelanggan.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -23,6 +25,23 @@ func CreatePelanggan(w http.ResponseWriter, r *http.Request) {
     _, err := collection.InsertOne(context.TODO(), pelanggan)
     if err != nil {
         http.Error(w, "Error creating pelanggan", http.StatusInternalServerError)
+        return
+    }
+
+    json.NewEncoder(w).Encode(pelanggan)
+}
+
+// GetPelangganByID fetches a single pelanggan by ID
+func GetPelangganByID(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    id, _ := primitive.ObjectIDFromHex(params["id"])
+
+    var pelanggan pelanggan.Pelanggan
+    collection := config.ConnectDB().Collection("pelanggan")
+
+    err := collection.FindOne(context.TODO(), bson.M{"_id": id}).Decode(&pelanggan)
+    if err != nil {
+        http.Error(w, "Pelanggan not found", http.StatusNotFound)
         return
     }
 
