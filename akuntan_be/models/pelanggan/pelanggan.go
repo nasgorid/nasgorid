2b1c17db41230442cb70c84ac5cd929d7d36e5d8
Wangjarimm <karamissuu@gmail.com>
2024-10-23 09:21:55 +0700

    add model pelanggan

---
+++ akuntan_be/models/pelanggan/pelanggan.go
@@ -0,0 +1,16 @@
+package pelanggan
+
+import (
+	"time"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Customer struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Name      string             `bson:"name" json:"name"`
+	Email     string             `bson:"email" json:"email"`
+	Phone     string             `bson:"phone" json:"phone"`
+	Address   string             `bson:"address" json:"address"`
+	CreatedAt time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt time.Time          `bson:"updatedAt" json:"updatedAt"`
+}
