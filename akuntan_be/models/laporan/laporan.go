4df5aad8c3236dbc100d30fc118dbfa4f45a3498
Wangjarimm <karamissuu@gmail.com>
2024-10-24 18:50:52 +0700

    edit model laporan

--- akuntan_be/models/laporan/laporan.go
+++ akuntan_be/models/laporan/laporan.go
@@ -1,11 +1,19 @@
 package laporan
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Laporan struct {
-    ID        string  `bson:"_id,omitempty" json:"id,omitempty"`
-    StartDate int64   `bson:"startDate" json:"startDate"`
-    EndDate   int64   `bson:"endDate" json:"endDate"`
-    Income    float64 `bson:"income" json:"income"`
-    Expenses  float64 `bson:"expenses" json:"expenses"`
-    Profit    float64 `bson:"profit" json:"profit"`
-    CreatedAt int64   `bson:"createdAt" json:"createdAt"`
+    ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+    StartDate  string             `bson:"startDate" json:"startDate"`   // Menggunakan string
+    EndDate    string             `bson:"endDate" json:"endDate"`       // Menggunakan string
+    StartDateTime time.Time       `bson:"startDateTime" json:"startDateTime"` // Menyimpan waktu yang sudah diparse
+    EndDateTime   time.Time       `bson:"endDateTime" json:"endDateTime"`
+	Income    float64   `bson:"income" json:"income"`
+	Expenses  float64   `bson:"expenses" json:"expenses"`
+	Profit    float64   `bson:"profit" json:"profit"`
+	CreatedAt time.Time `bson:"createdAt" json:"createdAt"`
 }
